96632a2c6c274e46d46d77c67741948cbf86f944
ChaseTeague <teaguechase@yahoo.com>
2024-01-28 21:25:07 -0500

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,25 @@
 // Your tests here
+import {isPalindrome} from '../utils';
+
+describe('isPalindrome', () => {
+    it("returns true for words that are a palindrome", () => {
+        const word = "racecar";
+        const question = isPalindrome(word);
+        expect(question).toBe(true);
+    })
+    it("return false for words that aren't a palindrome", () => {
+        const word = "car"
+        const question = isPalindrome(word)
+        expect(question).toBe(false);
+    })
+    it("return true for words that are a palindrome that are upper or lower case", () => {
+        const word = "RaCecAr"
+        const question = isPalindrome(word)
+        expect(question).toBe(true);
+    })
+    it("return false for empty string", () => {
+        const word = ""
+        const question = isPalindrome(word)
+        expect(question).toBe(false);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+export function isPalindrome(word){
+    if(word === ''){
+        return false;
+    }
+
+    word = word.toLowerCase();
+    word = word.replace(/[^a-z0-9]/g, '');
+
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
